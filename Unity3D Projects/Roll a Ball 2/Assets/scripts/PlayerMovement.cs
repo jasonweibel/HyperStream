c1ce7fee6c5df902733e13f766d54c2bcd3451a0
Jason Weibel <jason.weibel@pioneer.com>
2017-05-29 21:18:21 -0500

    learning unity and building out simple lessons for students

---
+++ Unity3D Projects/Roll a Ball 2/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
+
+    private Rigidbody _rb;
+    private int _count;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _count = _count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + _count.ToString();
+        if (_count >= 11)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
---
+++ Unity3D Projects/Simple Rolling Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text winText;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Treasure"))
+        {
+            other.gameObject.SetActive(false);
+            winText.text = "You Win!";
+        }
+    }
+}
